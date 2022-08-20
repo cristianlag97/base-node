0fb0d09bc18b40d31f15582279b4132db07b9328
Cristian Laguna <cdlaguna0@misena.edu.co>
2022-08-19 23:12:38 -0500

    google signin add

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user');
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -44,6 +45,57 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+
+  const { id_token } = req.body;
+
+  try {
+    // console.log( id_token );
+
+    const {name, picture, email} = await googleVerify(id_token);
+    // console.log({name,picture,email});
+
+    //* generar referencia para verificar si el correo ya existe en la DB *//
+    let user = await User.findOne({email});
+
+    if(!user) {
+      //* CREAR *//
+      const data = {
+        name,
+        email,
+        password: ':P',
+        img: picture,
+        google: true,
+      };
+      user = new User(data);
+      await user.save();
+    }
+
+    //* Verificar si está activo el usuario *//
+    if(!user.state) return res.status(401).json({
+      msg: 'Hable con el administrador, Usuario bloqueado'
+    });
+
+    //* generar JWT *//
+    const token = await generateJWT(user.id);
+
+    res.json({
+      estadoPeticion: true,
+      user,
+      token
+    })
+  } catch (error) {
+    console.log( error );
+    return res.status(400).json({
+      estadoPeticion: false,
+      msg: 'El token no se pudo verificar'
+    })
+  }
+
+
+}
+
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth.controller');
+const { login, googleSignIn } = require('../controllers/auth.controller');
 const { validateInput } = require('../middlewares/validate-inputs');
 
 const router = Router();
@@ -10,5 +10,10 @@ router.post('/login',[
   check('password', 'La contraseña es obligatoria').not().isEmpty(),
   validateInput
 ], login);
+
+router.post('/google',[
+  check('id_token', 'El token de Google es necesario').not().isEmpty(),
+  validateInput
+], googleSignIn);
 
 module.exports = router;
